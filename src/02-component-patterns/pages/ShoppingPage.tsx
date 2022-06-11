9575849a27121f8653244fa2a9663634e5f8c067
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-11 15:38:35 +0200

    91. Controlando el estado desde el padre

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -28,19 +28,22 @@ export const ShoppingPage = () => {
   const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCart }>({});
 
   const onProductCountChange = ({ count, product }: { count: number, product: Product}) => {
-    console.log({ count });
+    
     setShoppingCart( oldShoppingCart => {
-      if ( count === 0 ) {
-        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+
+      const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+
+      if ( Math.max( productInCart.count + count, 0) > 0 ) {
+        productInCart.count += count;
         return {
-          ...rest
+          ...oldShoppingCart,
+          [product.id]: productInCart
         }
       }
 
-      return {
-        ...oldShoppingCart,
-        [product.id]: { ...product, count }
-      };
+      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+      return { ...rest }
+
     });
   };
 
