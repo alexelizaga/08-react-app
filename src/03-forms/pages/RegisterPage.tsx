a3f4314638e2fb6df5e7cf37f405fb6bc5037b80
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-14 07:06:49 +0200

    127. Traditional form - with useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,31 +1,55 @@
+import { ChangeEvent, useState } from 'react';
+
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
-  return (
-    <div>
-        <h1>Register Page</h1>
 
-        <form>
-            <input
-                type="text"
-                placeholder="Name"
-            />
-            <input
-                type="email"
-                placeholder="Name"
-            />
-            <input
-                type="password"
-                placeholder="Password"
-            />
-            <input
-                type="password"
-                placeholder="Repeat Password"
-            />
+    const [registerData, setRegisterData] = useState({
+        name: 'Alex',
+        email: 'alex@email.com',
+        password1: '123456',
+        password2: '123456',
+    });
 
-            <button type="submit">Create</button>
+    const { name, email, password1, password2 } = registerData;
 
-        </form>
-    </div>
-  )
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    }
+
+    return (
+        <div>
+            <h1>Register Page</h1>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={ name }
+                    onChange={ onChange }
+                />
+                <input
+                    type="email"
+                    placeholder="Name"
+                    value={ email }
+                    onChange={ onChange }
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={ password1 }
+                    onChange={ (ev) => onChange(ev) }
+                />
+                <input
+                    type="password"
+                    placeholder="Repeat Password"
+                    value={ password2 }
+                    onChange={ onChange }
+                />
+
+                <button type="submit">Create</button>
+
+            </form>
+        </div>
+    )
 }
