7b15ca5bbf04fe2af4c040500b8e2a960b8db87f
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-14 07:41:17 +0200

    129. CustomHook - useForm

---
+++ src/03-forms/hooks/useForm.ts
@@ -0,0 +1,22 @@
+import { ChangeEvent, useState } from 'react';
+
+export const useForm = <T>( initialState: T ) => {
+
+    const [formData, setFormData] = useState( initialState );
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormData( prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }) );
+
+    }
+
+    return {
+        ...formData,
+        // properties
+        formData,
+        // methods
+        onChange
+    }
+}
