a6439b6e5e245dd2da0267155bf88caf5b45041a
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-15 07:56:31 +0200

    131. Formik - Basic Exercise

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
+import { FormikBasicPage } from '../03-forms/pages/FormikBasicPage';
 import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 import logo from '../logo.svg';
@@ -12,31 +13,33 @@ export const Navigation = () => {
             <nav>
                 <img src={ logo } alt='React Logo' />
                 <ul>
+                    
                     <li>
                         <NavLink
                             to='/'
                             className={ ({ isActive }) => isActive ? 'nav-active' : '' }
                         >Home</NavLink>
                     </li>
-                    <li>
-                        <NavLink
-                            to='/about'
-                            className={ ({ isActive }) => isActive ? 'nav-active' : '' }
-                        >About</NavLink>
-                    </li>
+                    
                     <li>
                         <NavLink
                             to='/register'
                             className={ ({ isActive }) => isActive ? 'nav-active' : '' }
                         >Register Page</NavLink>
                     </li>
+                    <li>
+                        <NavLink
+                            to='/formik-basic'
+                            className={ ({ isActive }) => isActive ? 'nav-active' : '' }
+                        >Formik basic</NavLink>
+                    </li>
                 </ul>
             </nav>
 
             <Routes>
                 <Route path='home' element={ <h1>Home</h1> } />
-                <Route path='about' element={ <h1>About</h1> } />
                 <Route path='register' element={ <RegisterPage /> } />
+                <Route path='formik-basic' element={ <FormikBasicPage /> } />
 
                 <Route path='/*' element={ <Navigate to="/home" replace /> } />
             </Routes>
