704ec3219255cbffda1f92f74a62d2290121bdeb
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-16 20:27:54 +0200

    151. Dynamic Validation

--- src/03-forms/pages/DynamicFormPage.tsx
+++ src/03-forms/pages/DynamicFormPage.tsx
@@ -1,14 +1,30 @@
-import { Formik, Form } from 'formik'; 
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 import { MySelect, MyTextInput } from '../components';
 import formJson from '../data/custom-form.json';
 
 
 const initialValues: { [key: string ]: any } = {}
+const requiredFields: { [key: string ]: any } = {}
 
 for ( const input of formJson ) {
   initialValues[input.name] = input.value;
+
+  if ( !input.validations ) continue;
+
+  let schema = Yup.string()
+
+  for ( const rule of input.validations ) {
+    if ( rule.type === 'required' ) {
+      schema = schema.required('Required')
+    }
+  }
+
+  requiredFields[input.name] = schema;
 }
+
+const validationSchema = Yup.object({ ...requiredFields });
 
 export const DynamicFormPage = () => {
   return (
@@ -20,6 +36,7 @@ export const DynamicFormPage = () => {
           onSubmit={ (values) => {
             console.log(values);
           }}
+          validationSchema={ validationSchema }
         >
           { (formik) => (
             <Form noValidate>
